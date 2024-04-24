679c86531ea6fad4d3eb2948d375e39e8a7dbbef
Zahraa_Alsous <alsous.zahraa@gmail.com>
2024-04-24 12:21:05 +0300

    validation order

--- app/Http/Controllers/Api_out_user/OrderController.php
+++ app/Http/Controllers/Api_out_user/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api_out_user;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Order;
 //use App\Models\User;
 //use App\Models\Appointment;
@@ -14,6 +15,26 @@ class OrderController extends Controller
 {
     public function CreateOrderForJoinToSchool(Request $request)
     {
+
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'father_name' => 'required|string',
+            'birthday' => 'required|date',
+            'gender' => 'required|in:0,1',
+            'phone' => 'required|string',
+            'address' => 'required|string',
+            'email' => 'required|email',
+            'classification' => 'required|in:0,1',
+            'class' => 'required|string',
+            'year' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+
             $new = new Order;
 
             $new->first_name = $request->first_name;
@@ -28,6 +49,8 @@ class OrderController extends Controller
             $new->class = $request->class;
             $new->year = $request->year;
             $new->save();
+
+            return response()->json(['message' => 'Order created successfully'], 200);
     }
 
     public function CreateOrderForCourse(Request $request, $course_id)
